8ef63e0dde467d8e25cd99f4a1c45f20a49809ef
Amol jadhav <149037157+amolj1103@users.noreply.github.com>
2023-12-28 12:07:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+console.log(process.env)
+
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
