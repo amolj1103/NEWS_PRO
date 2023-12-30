a7091f9ba9185bf3f208337cfe63a0a6c1e77f2d
amolj1103 <amolj2332@gmail.com>
2023-12-30 21:54:53 +0530

    adding the base files

---
+++ client/precache-manifest.05515b7fe1c59cca21ac8dc11f8a7df2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee52a6fbfb958611697731534a5b0a9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d0d115f034c052dd471",
+    "url": "/static/css/2.d07f5756.chunk.css"
+  },
+  {
+    "revision": "f9636f28fdbdb82d7fb8",
+    "url": "/static/css/main.9bd278d4.chunk.css"
+  },
+  {
+    "revision": "2d0d115f034c052dd471",
+    "url": "/static/js/2.cbf8b242.chunk.js"
+  },
+  {
+    "revision": "2ad33e9abe21e6769bbc",
+    "url": "/static/js/3.bd282c0b.chunk.js"
+  },
+  {
+    "revision": "f9636f28fdbdb82d7fb8",
+    "url": "/static/js/main.30750b66.chunk.js"
+  },
+  {
+    "revision": "02558d794f1b309512ce",
+    "url": "/static/js/runtime~main.f27e42ef.js"
+  },
+  {
+    "revision": "8ef9bbf7d6280922fac3529e6ce20cd3",
+    "url": "/static/media/Background.8ef9bbf7.jpg"
+  },
+  {
+    "revision": "1931427c23716bb4ff98d03627f7137c",
+    "url": "/static/media/fa-brands-400.1931427c.ttf"
+  },
+  {
+    "revision": "b6356c957274676e6571c1ff5e11c9a8",
+    "url": "/static/media/fa-brands-400.b6356c95.woff2"
+  },
+  {
+    "revision": "023a4a925fa3fce0f66b769ef6bbb264",
+    "url": "/static/media/fa-regular-400.023a4a92.woff2"
+  },
+  {
+    "revision": "a0cc1c8265e3163aa654a5284ea11ace",
+    "url": "/static/media/fa-regular-400.a0cc1c82.ttf"
+  },
+  {
+    "revision": "25914cfeafced317e1a0372187fbb2b9",
+    "url": "/static/media/fa-solid-900.25914cfe.ttf"
+  },
+  {
+    "revision": "6c4eee562650e53cee32496bdfbe534b",
+    "url": "/static/media/fa-solid-900.6c4eee56.woff2"
+  },
+  {
+    "revision": "71808e147dc6d82c198a4ba292c0cb69",
+    "url": "/static/media/fa-v4compatibility.71808e14.ttf"
+  },
+  {
+    "revision": "c9e50ccabe9e36f370272197595ea1e5",
+    "url": "/static/media/fa-v4compatibility.c9e50cca.woff2"
+  },
+  {
+    "revision": "fd77f8e6efd341eb83a3c40bff8737f5",
+    "url": "/static/media/loading.fd77f8e6.gif"
+  }
+]);
