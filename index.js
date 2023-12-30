38e5d2c1bb1867b65c8f414edf09a96965bb3bac
Amol jadhav <149037157+amolj1103@users.noreply.github.com>
2023-12-30 12:53:37 +0000

    adding the changes

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const path = require("path")
+const port = process.env.PORT || 5000
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.static(path.join(__dirname, 'client')))
 
 app.get("/*",  (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, 'client', 'index.html'))
 })
 
 app.listen(port, () => {
